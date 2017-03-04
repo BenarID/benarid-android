8e95ce35eefda5ce54d727a26a7e70d6c7354d29
Bobby Priambodo <bobby.priambodo@gmail.com>
2017-03-04 13:40:15 +0700

    Use RecyclerView for portals list

--- app/src/main/java/id/benar/benarid/PortalsActivity.java
+++ app/src/main/java/id/benar/benarid/PortalsActivity.java
@@ -2,13 +2,73 @@ package id.benar.benarid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class PortalsActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portals);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.list_portals);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String[] dataset = new String[5];
+        dataset[0] = "Detik";
+        dataset[1] = "Kompas";
+        dataset[2] = "CNN Indonesia";
+        dataset[3] = "Metrotvnews";
+        dataset[4] = "BBC Indonesia";
+
+        mAdapter = new PortalsListAdapter(dataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
+}
+
+class PortalsListAdapter extends RecyclerView.Adapter<PortalsListAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    public PortalsListAdapter(String[] dataset) {
+        mDataset = dataset;
+    }
+
+    @Override
+    public PortalsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_portal, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
 }
